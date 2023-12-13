15a5ba7ea3afbcafa7a3b1c660eca33ecd967908
truongnguyen5x <truongnguyen5x@gmail.com>
2023-12-13 15:31:28 +0700

    update serrver

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,28 @@
 export default [
-  'strapi::cors',
-   'strapi::security',
-   'strapi::errors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::cors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          "script-src": ["'self'", "'unsafe-inline'", "cdn.jsdelivr.net"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "https:",
+            "cdn.jsdelivr.net",
+            "strapi.io",
+          ],
+        },
+      },
+    },
+  },
+  "strapi::errors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
