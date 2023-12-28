bcccb9e02e902d7a9f6eeb6b472a8999cf670eb9
truongnguyen5x <truongnguyen5x@gmail.com>
2023-12-28 14:45:07 +0000

    fix

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1339),
-  url: env('PUBLIC_URL', 'http://178.128.59.133:1340'),
+  url: env('PUBLIC_URL', 'http://178.128.59.133:1339'),
   app: {
     keys: env.array("APP_KEYS"),
   },
