f8913dd24b089243de42f65b72aad6b178f46e03
truongnguyen5x <truongnguyen5x@gmail.com>
2023-12-28 04:48:20 +0000

    fix

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  url: env('PUBLIC_URL', 'https://dev-log-portal-cms.viettelpost.vn'),
+  port: env.int("PORT", 1339),
+  url: env('PUBLIC_URL', 'http://178.128.59.133:1340'),
   app: {
     keys: env.array("APP_KEYS"),
   },
